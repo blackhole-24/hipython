c8ff493a3cf9e71f40806a10e01269a0cf4d31ba
blackhole <blackhole1247@gmail.com>
2026-02-25 10:30:47 +0900

    webcrawl567

---
+++ webcrawlling/4.py
@@ -0,0 +1,31 @@
+# beautifulsoup으로 파싱
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+
+with sync_playwright() as p:
+  browser = p.chromium.launch(headless=False)
+  page = browser.new_page()
+  page.goto("http://quotes.toscrape.com/")
+
+  html = page.content()
+  soup = BeautifulSoup(html, 'lxml')
+  # print(soup.find('span', class_= 'text'))
+  print(soup.select_one('span.text').text)
+  print(soup.select_one('small.author').text)
+
+  print(soup.select('div.quote'))
+  quotes = soup.select('div.quote') #리스트로 변환
+  quotes_list = []
+  for quote in quotes:
+    quotes_list.append({'quote': quote.select_one('span.text').text,
+                       'author': quote.select_one('small.author').text})
+    
+  import pandas as pd
+  df = pd.DataFrame(quotes_list)
+  print(df.head())
+  #print(quotes_list)
+
+
+
+
+
